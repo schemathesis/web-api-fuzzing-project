f203a0df4d5d2f4cb65cae39fd82283883422679
Dmitry Dygalo <dadygalo@gmail.com>
2021-04-10 14:44:18 +0200

    chore: Create a proper auth token for `cccatalog_api`

--- src/wafp/targets/catalog/cccatalog_api/__init__.py
+++ src/wafp/targets/catalog/cccatalog_api/__init__.py
@@ -1,3 +1,9 @@
+import re
+import time
+from typing import Dict
+
+import requests
+
 from wafp.targets import (
     BaseTarget,
     Language,
@@ -29,3 +35,32 @@ class Default(BaseTarget):
             ),
             specification=Specification(name=SpecificationType.OPENAPI, version="2.0"),
         )
+
+    def after_start(self, stdout: bytes, headers: Dict[str, str]) -> None:
+        # Create auth token for the `Authorization` header
+        base_url = self.get_base_url()
+        credentials_response = requests.post(
+            f"{base_url}/auth_tokens/register",
+            json={"name": "test", "description": "TEST", "email": "test@test.com"},
+        )
+        data = credentials_response.json()
+        response = requests.post(
+            f"{base_url}/auth_tokens/token/",
+            data={
+                "client_id": data["client_id"],
+                "client_secret": data["client_secret"],
+                "grant_type": "client_credentials",
+            },
+        )
+        token_data = response.json()
+        token = token_data["access_token"]
+        token_type = token_data["token_type"]
+        headers["Authorization"] = f"{token_type} {token}"
+        # Follow the link in the email to avoid throttling API requests
+        deadline = time.time() + 10
+        for line in self.compose.log_stream(deadline=deadline):
+            match = re.search(f" ({base_url}/auth_tokens/verify/.+)$".encode(), line)
+            if match is not None:
+                token_verification_url = match.groups()[0].decode().strip()
+                requests.get(token_verification_url)
+                break

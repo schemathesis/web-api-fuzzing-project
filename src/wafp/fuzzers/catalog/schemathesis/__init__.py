9b9783cf0b9e27ea1e9494e06f6056cb90fb7aa2
Dmitry Dygalo <dadygalo@gmail.com>
2021-04-18 16:08:32 +0200

    feat: Add Schemathesis new-style stateful approach

--- src/wafp/fuzzers/catalog/schemathesis/__init__.py
+++ src/wafp/fuzzers/catalog/schemathesis/__init__.py
@@ -1,6 +1,8 @@
-from typing import Dict, List
+from textwrap import dedent
+from typing import Dict, List, Union
 
 from wafp.fuzzers import BaseFuzzer, FuzzerContext
+from wafp.utils import NotSet
 
 
 class Default(BaseFuzzer):
@@ -37,3 +39,48 @@ class StatefulOld(Default):
         args = super().get_entrypoint_args(context, schema, base_url, headers)
         args.append("--stateful=links")
         return args
+
+
+class StatefulNew(BaseFuzzer):
+    def get_entrypoint(self) -> Union[str, NotSet]:
+        return "pytest"
+
+    def get_entrypoint_args(
+        self, context: FuzzerContext, schema: str, base_url: str, headers: Dict[str, str]
+    ) -> List[str]:
+        filename = "test_stateful.py"
+        test_file = context.input_directory / filename
+        test_file.write_text(
+            dedent(
+                f"""
+import json
+import os
+from urllib.parse import urlparse
+
+import schemathesis
+from hypothesis import settings, HealthCheck
+from schemathesis.checks import not_a_server_error
+
+if urlparse("{schema}").scheme != "":
+    loader = schemathesis.from_uri
+else:
+    loader = schemathesis.from_path
+
+schema = loader("{schema}", base_url="{base_url}", validate_schema=False)
+
+class APIWorkflow(schema.as_state_machine()):
+
+    def get_call_kwargs(self, case):
+        return {{"headers": {headers}}}
+
+    def validate_response(self, response, case, additional_checks=()):
+        case.validate_response(
+            response, checks=(not_a_server_error,), additional_checks=additional_checks
+        )
+
+TestCase = APIWorkflow.TestCase
+TestCase.settings = settings(max_examples=100, suppress_health_check=HealthCheck.all(), deadline=None)
+"""
+            )
+        )
+        return [str(self.get_container_input_directory() / filename)]

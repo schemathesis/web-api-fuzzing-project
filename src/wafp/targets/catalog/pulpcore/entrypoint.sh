218ff332ca315628f8cf395f7d07d00d60d45658
Dmitry Dygalo <dadygalo@gmail.com>
2021-07-01 19:31:09 +0200

    chore: Wait for Postgres in pulpcore
    Signed-off-by: Dmitry Dygalo <dadygalo@gmail.com>

--- src/wafp/targets/catalog/pulpcore/entrypoint.sh
+++ src/wafp/targets/catalog/pulpcore/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+# Wait for Postgres
+{
+  while ! echo -n > /dev/tcp/db/5432;
+  do
+    sleep 0.1;
+  done;
+} 2>/dev/null
+
 pulpcore-manager migrate --noinput
 pulpcore-manager reset-admin-password --password test
 

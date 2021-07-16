40b0bfb745d75ca0786aac87c8a16ae5a9c1ba14
Dmitry Dygalo <dadygalo@gmail.com>
2021-07-16 17:44:21 +0200

    chore: Add linked schema for Worklog
    Signed-off-by: Dmitry Dygalo <dadygalo@gmail.com>

--- src/wafp/targets/catalog/worklog/__init__.py
+++ src/wafp/targets/catalog/worklog/__init__.py
@@ -15,3 +15,34 @@ class Default(BaseTarget):
         return Metadata.flasgger(
             flask_version="1.0.2", flasgger_version="0.9.1", openapi_version="2.0", validation_from_schema=False
         )
+
+
+class Linked(Default):
+    def get_schema_location(self) -> str:
+        # API schema with the following links:
+        #  PUT /user/create -> DELETE /user/{username}
+        #  PUT /user/create -> GET /user/{username}
+        #  PUT /user/create -> POST /user/{username}
+        #  PUT /user/create -> PUT /user/{username}
+        #  PUT /user/create -> PUT /user/{username}/reset
+        #  DELETE /user/{username} -> DELETE /user/{username}
+        #  DELETE /user/{username} -> GET /user/{username}
+        #  DELETE /user/{username} -> POST /user/{username}
+        #  DELETE /user/{username} -> PUT /user/{username}
+        #  DELETE /user/{username} -> PUT /user/{username}/reset
+        #  GET /user/{username} -> DELETE /user/{username}
+        #  GET /user/{username} -> GET /user/{username}
+        #  GET /user/{username} -> POST /user/{username}
+        #  GET /user/{username} -> PUT /user/{username}
+        #  GET /user/{username} -> PUT /user/{username}/reset
+        #  POST /user/{username} -> DELETE /user/{username}
+        #  POST /user/{username} -> GET /user/{username}
+        #  POST /user/{username} -> POST /user/{username}
+        #  POST /user/{username} -> PUT /user/{username}
+        #  POST /user/{username} -> PUT /user/{username}/reset
+        #  PUT /user/{username}/reset -> DELETE /user/{username}
+        #  PUT /user/{username}/reset -> GET /user/{username}
+        #  PUT /user/{username}/reset -> POST /user/{username}
+        #  PUT /user/{username}/reset -> PUT /user/{username}
+        #  PUT /user/{username}/reset -> PUT /user/{username}/reset
+        return str(self.path / "schema-with-links.json")

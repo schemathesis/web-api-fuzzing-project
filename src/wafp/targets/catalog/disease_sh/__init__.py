3c61c1dae8a34386fca40baa34bb8fb46ce0eaa7
Dmitry Dygalo <dadygalo@gmail.com>
2021-07-27 18:26:29 +0200

    chore: Add a linked schema to Disease.sh
    Signed-off-by: Dmitry Dygalo <dadygalo@gmail.com>

--- src/wafp/targets/catalog/disease_sh/__init__.py
+++ src/wafp/targets/catalog/disease_sh/__init__.py
@@ -28,3 +28,21 @@ class Default(BaseTarget):
             specification=Specification(name=SpecificationType.OPENAPI, version="2.0"),
             validation_from_schema=False,
         )
+
+
+class Linked(Default):
+    def get_schema_location(self) -> str:
+        # API schema with the following links:
+        #  - GET /v3/covid-19/gov/ -> GET /v3/covid-19/gov/{country}
+        #  - GET /v3/covid-19/apple/countries => GET /v3/covid-19/apple/countries/{country}
+        #  - GET /v3/covid-19/apple/countries/{country} => GET /v3/covid-19/apple/countries/{country}/{subregions}
+        #  - GET /v3/covid-19/nyt/counties -> GET /v3/covid-19/nyt/counties/{county}
+        #  - GET /v3/covid-19/nyt/states -> GET /v3/covid-19/nyt/states/{state}
+        #  - GET /v3/covid-19/historical/usacounties -> GET /v3/covid-19/historical/usacounties/{state}
+        #  - GET /v3/covid-19/historical -> GET /v3/covid-19/historical/{country}
+        #  - GET /v3/covid-19/historical/{country} -> GET /v3/covid-19/historical/{country}/{province}
+        #  - GET /v3/covid-19/jhucsse/counties -> GET /v3/covid-19/jhucsse/counties/{county}
+        #  - GET /v3/covid-19/countries -> GET /v3/covid-19/countries/{country}
+        #  - GET /v3/covid-19/continents -> /v3/covid-19/continents/{continent}
+        #  - GET /v3/covid-19/states -> /v3/covid-19/states/{states}
+        return str(self.path / "schema-with-links.json")
